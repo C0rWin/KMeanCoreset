c4ace2d8e830ad618401a1d81b694f8419c36504
Artem Barger <artem@bargr.net>
2017-06-06 09:23:08 +0300

    Re-organize project structure, group file into separate folder
    Signed-off-by: Artem Barger <artem@bargr.net>

--- computeEnergy.m
+++
@@ -1,7 +0,0 @@
-function energy = computeEnergy(points, centers, k)
-    dist = zeros(size(points,1), k);
-    for c=1:k
-        dist(:,c) = sum( bsxfun(@minus, points, centers(c,:)).^2, 2);
-    end         
-    energy = sum(min(dist, [], 2));
-end
